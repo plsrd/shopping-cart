811a065e553c1ac68dc885b3b5fcd7abc3ed4e1f
racheallarimer <racheal.larimer@gmail.com>
2021-03-12 12:57:04 -0800

    move carousel to shop page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Carousel from '../components/Carousel'
 
 const Home = () => {
 
   return (
     <div>
-      <Carousel />
     </div>
   )
 }
--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Carousel from './Carousel'
 
 const Shop = () => {
 
   return (
     <div>
-
+        <h2>Featured Items</h2>
+        <Carousel />
     </div>
   )
 }
